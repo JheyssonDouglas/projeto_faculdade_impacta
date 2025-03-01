e6b991ecb402060e4dc64f17676ddd64592af50a
Jheysson Douglas <jheyssonx@gmail.com>
2025-02-28 23:40:41 -0300

    Atualizando lista de produtos

--- gestao_restaurantes/restaurante/models.py
+++ gestao_restaurantes/restaurante/models.py
@@ -5,6 +5,8 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/', blank=True, null=True)
+    category = models.CharField(max_length=100, default="")  
+    subcategory = models.CharField(max_length=100, default="")  
 
     def __str__(self):
         return self.name
